5c230e2815cf6cecc16a981c3f2e108be75eca0b
Konrad Nowakowski <konrad.x92@gmail.com>
2021-03-19 14:06:33 +0000

    Add enemy utility functions

--- src/units/base_enemy/base_enemy.gd
+++ src/units/base_enemy/base_enemy.gd
@@ -1,6 +1,23 @@
 extends "res://units/base_unit/base_unit.gd"
 
-signal hitted()
+export(float) var height: float = 20.0;
 
 func hit() -> void:
-	emit_signal("hitted")
+	pass
+
+func kill(delay: float) -> void:
+	dispose(delay)
+	freeze()
+
+func freeze() -> void:
+	set_process(false)
+	set_physics_process(false)
+	set_collision_mask(0x0)
+	set_collision_layer(0x0)
+
+func dispose(delay: float) -> void:
+	var timer = get_tree().create_timer(delay)
+	timer.connect("timeout", self, "queue_free")
+
+func _on_body_entered(body: PhysicsBody2D) -> void:
+	if body.is_in_group("player"): hit()
