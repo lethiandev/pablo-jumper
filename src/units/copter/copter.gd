87603f9919720d00a0bfb3012795e5252b3acdd2
Konrad Nowakowski <konrad.x92@gmail.com>
2021-03-19 21:12:52 +0000

    Add target amplitudes for copter unit

--- src/units/copter/copter.gd
+++ src/units/copter/copter.gd
@@ -1,14 +1,29 @@
 extends "res://units/base_enemy/base_enemy.gd"
 
-var ticks = 0.0
+export var min_amplitude: float = 50.0
+export var max_amplitude: float = -50.0
+export var direction: float = 1.0
+
+const ACCELERATION = 200.0
+const MAX_SPEED = 50.0
+
+var original_position = Vector2()
+
+func _ready():
+	original_position = global_position
 
 func _physics_process(delta: float) -> void:
+	# Movement direction based on amplitude
+	var yrel = global_position.y - original_position.y
+	if yrel > min_amplitude: direction = -1.0
+	if yrel < max_amplitude: direction = 1.0
+	
 	# Vertical movement
-	linear_velocity.y += cos(ticks)
-	ticks += 2.0 * delta
+	linear_velocity.y += ACCELERATION * direction * delta
+	linear_velocity.y = clamp(linear_velocity.y, -MAX_SPEED, MAX_SPEED)
 
-# Disable gravity acceleration
 func _physics_process_gravity(delta: float) -> void:
+	# Override gravity acceleration by none
 	pass
 
 func hit() -> void:
