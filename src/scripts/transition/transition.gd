b0cdd30c50db1f5879a3edfcf3c1fecaffa77abb
Konrad Nowakowski <konrad.x92@gmail.com>
2021-03-21 13:12:27 +0000

    Refactor transition effect

--- src/scripts/transition/transition.gd
+++ src/scripts/transition/transition.gd
@@ -6,28 +6,26 @@ signal curtain_opened()
 func _ready() -> void:
 	$ColorRect.visible = false
 
-func transit_fade_out() -> void:
-	transit("fade_out")
+func fade_out() -> void:
+	yield(transit("fade_out"), "completed")
 
-func transit_fade_in() -> void:
-	transit("fade_in")
+func fade_in() -> void:
+	yield(transit("fade_in"), "completed")
 
 func transit(anim_name: String) -> void:
 	get_tree().set_pause(true)
 	$ColorRect/AnimationPlayer.play(anim_name)
 	$ColorRect.visible = true
+	
+	# Wait for animation to end
+	yield($ColorRect/AnimationPlayer, "animation_finished")
 
 func _on_animation_finished(anim_name: String) -> void:
 	if anim_name == "fade_out":
 		emit_signal("curtain_closed")
-		_after_curtain_close()
 	if anim_name == "fade_in":
 		emit_signal("curtain_opened")
 		_after_curtain_open()
-
-func _after_curtain_close() -> void:
-	# Workaround for signal not emitted in queue mode
-	$ColorRect/AnimationPlayer.play("fade_in")
 
 func _after_curtain_open() -> void:
 	get_tree().set_pause(false)
