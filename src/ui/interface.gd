31aa04d5d4a9b84d63a3e957edef7baec80de945
Konrad Nowakowski <konrad.x92@gmail.com>
2021-03-21 18:01:19 +0000

    Fix interface layout

--- src/ui/interface.gd
+++ src/ui/interface.gd
@@ -1,9 +1,9 @@
 extends CanvasLayer
 
-onready var score_format = $VBoxContainer/Score.text
+onready var score_format = $Container/Score.text
 
 func _process(delta: float) -> void:
 	var score = GameState.current_score
 	var energy = GameState.energy
-	$VBoxContainer/Score.text = score_format % score
-	$VBoxContainer/Control/EnergyBar.energy = energy
+	$Container/Score.text = score_format % score
+	$Container/EnergyBar.energy = energy
