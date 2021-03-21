0eeeb7112219f0aeb25f7f98e9f82eecae28b923
Konrad Nowakowski <konrad.x92@gmail.com>
2021-03-21 12:21:20 +0000

    Add door lock feature

--- src/props/door/door.gd
+++ src/props/door/door.gd
@@ -1,7 +1,18 @@
 extends Node2D
 
+export var locked: bool = false
 export var opened: bool = false \
 	setget set_opened, is_open
+
+func _process(delta: float) -> void:
+	if locked and is_open():
+		close()
+
+func lock() -> void:
+	locked = true
+
+func unlock() -> void:
+	locked = false
 
 func open() -> void:
 	set_opened(true)
