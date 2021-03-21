7eba18c20ae71ccc85b59b32bc887421e97d635f
Konrad Nowakowski <konrad.x92@gmail.com>
2021-03-21 13:12:54 +0000

    Add yield feature on camera shake effect

--- src/scripts/camera/camera.gd
+++ src/scripts/camera/camera.gd
@@ -6,7 +6,14 @@ func _process(delta: float) -> void:
 func shake_low() -> void:
 	$ShakeEffect.amplitude = 12.0
 	$ShakeEffect.shake(0.5, 30.0)
+	# Wait for shake effect to end
+	yield(_wait(0.5), "completed")
 
 func shake_high() -> void:
 	$ShakeEffect.amplitude = 28.0
 	$ShakeEffect.shake(0.8, 50.0)
+	# Wait for shake effect to end
+	yield(_wait(0.8), "completed")
+
+func _wait(time: float) -> void:
+	yield(get_tree().create_timer(time), "timeout")
